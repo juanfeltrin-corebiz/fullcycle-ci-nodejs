d873b5bbcc024cf5504b578db7abdbf8031d94c2
Juan Feltrin <juan.feltrin@corebiz.ag>
2021-07-26 17:18:50 -0300

    ci: teste para o ci

--- index.test.js
+++ index.test.js
@@ -2,6 +2,6 @@ const tape = require('tape')
 const index = require('./index')
 
 tape('Somar entre dois números', (t) => {
-    t.assert(index.soma(5, 5) === 10, "Somou corretamente")
+    t.assert(index.soma(5, 5) === 10, "Somou corretamente!")
     t.end()
 })
